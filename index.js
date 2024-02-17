4e368d782307233c40c92a732561a24cb1925711
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-02-17 09:51:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const Discord = require('discord.js');
+const { Client, Collection, Intents } = Discord;
+const handler = require("./src/handlers/index");
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+    ],
+});
 
 
 // ... le reste de votre code ...
